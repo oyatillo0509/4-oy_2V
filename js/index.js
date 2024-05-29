bdd5fabf17f2da1242451b906dabf3543ad82c9f
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 12:50:47 +0500

    finishd

---
+++ js/index.js
@@ -0,0 +1,207 @@
+// 1-masala
+
+// let a=+prompt("Son kiriting");
+// if(a>0){
+//     a++;
+// }
+// console.log(a);
+
+// 2-masala
+
+// let a=+prompt("Ixtiyoriy son kiriting")
+// if(a>0){
+//     a++;
+// }else{
+//     a-=2;
+// }
+// console.log(a);
+
+// 3-masala
+
+// let a=+prompt("Son kiriting");
+// if(a>0){
+//     a++;
+// }
+// else if(a<0){
+//     a-=2;
+// } else{
+//     a=10;
+// }
+// console.log(a);
+
+// 4-masala
+
+// let a=+prompt("1-sonni kiriting");
+// let b=+prompt("2-sonni kiriting");
+// let c=+prompt("3-sonni kiriting");
+// let sum=0;
+// if(a>0){
+//     sum++;
+// }else if(b>0){
+//     sum++;
+// } else if(c>0){
+//     sum++;
+// }
+// console.log(sum);
+
+// 5-masala
+
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// let c = +prompt("3-son");
+// let musbatCount = 0;
+// let manfiyCount = 0;
+
+// if (a > 0) {
+//   musbatCount++;
+// } else {
+//   manfiyCount++;
+// }
+
+// if (b > 0) {
+//   musbatCount++;
+// } else {
+//   manfiyCount++;
+// }
+
+// if (c > 0) {
+//   musbatCount++;
+// } else {
+//   manfiyCount++;
+// }
+
+// console.log("musbat" +musbatCount );
+// console.log("manfiy" +manfiyCount );
+
+// 6-masala
+
+// let a = +prompt("1-sonni kiriting"),
+//   b = +prompt("2-sonni kiriting");
+// if (a > b) {
+//   console.log("1-son katta -> "+a);
+// } else {
+//   console.log("2-son katta -> "+ b);
+// }
+
+// 7-masala
+
+// let a = +prompt("1-sonni kiriting"),
+//   b = +prompt("2-sonni kiriting");
+// if (a > b) {
+//   console.log("1-son katta");
+// } else {
+//   console.log("2-son katta");
+// }
+
+// 8-masala
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+
+// let max = 0;
+// let min = 0;
+
+// if (a > b) {
+//   max = a;
+//   min = b;
+// } else if (a == b) {
+//   max = "1- va 2- son o'zaro";
+//   min = "1- va 2- son o'zaro";
+// } else {
+//   max = b;
+//   min = a;
+// }
+
+// console.log(max);
+// console.log(min);
+
+// 9-masala
+
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// let c;
+
+// if(a>b){
+//   c=a;
+//   a=b;
+//   b=c
+// }
+// console.log("B ->"+b);
+// console.log("A ->"+a);
+
+// 10-masala
+// let a = +prompt("A-sonni kiriting"),
+//   b = +prompt("B-sonni kiriting A sondan kichik bo'lsin!");
+// if(a!=b){
+//     a=a+b;
+//     b=a+b;
+// }else{
+//     a=0;
+//     b=0;
+// }
+// console.log("A = "+a);
+// console.log("B = "+b);
+
+// 11-masala
+
+// let a = +prompt("1-son");
+// let b = +prompt("2-son");
+// let c;
+// if(a==b){
+//   a=0;
+//   b=0;
+// }
+// else if(a>b){
+//   c=a;
+//   a=c;
+//   b=c;
+// }
+// else{
+//   c=b;
+//   a=c;
+//   b=c;
+// }
+
+// console.log(a);
+// console.log(b);
+
+// 12-masala
+
+// let a=+prompt("1-sonni kiriting"),
+//     b=+prompt("2-sonni kiriting"),
+//     c=+prompt("3-sonni kiriting");
+// if(a<b & b<c){
+//     console.log(a);
+// } else if(b<a & a<c){
+//     console.log(b);
+// }else if(b>a & c>a){
+//     console.log(b);
+// }
+// else{
+//     console.log(c);
+// }
+
+// 13-masala
+
+// let a=+prompt("1-sonni kiriting"),
+//     b=+prompt("2-sonni kiriting"),
+//     c=+prompt("3-sonni kiriting"),
+//     sum=null;
+// sum=(a+b+c)/3;
+// console.log(sum);
+
+// 14-masala
+
+// let a=+prompt("1-sonni kiriting"),
+//     b=+prompt("2-sonni kiriting"),
+//     c=+prompt("3-sonni kiriting");
+
+// if(a>b & b>c){
+//     console.log(a);
+//     console.log(c);
+// }else if(b>a & a>c){
+//     console.log(b);
+//     console.log(c);
+// }else if(c>a && a>b){
+//     console.log(c);
+//     console.log(b);
+// }
